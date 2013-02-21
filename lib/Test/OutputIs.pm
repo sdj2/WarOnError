6cff4cf4dcbd416ada991ea40d065ba7e9a1f736
Seth Johnson <seth.johnson@gmail.com>
2013-02-20 23:43:48 -0800

    Create module for testing what a function (closure) prints to STDOUT

---
+++ lib/Test/OutputIs.pm
@@ -0,0 +1,30 @@
+package Test::OutputIs;
+
+use warnings;
+use strict;
+use Test::More;
+use IO::String;
+use Exporter qw(import);
+
+our @EXPORT = qw(output_is);
+our @EXPORT_OK = qw(output_is);
+
+# is() for STDOUT produced by closure
+sub output_is {
+	my $closure = shift;
+	my $expected = shift;
+	my $testName = shift;
+
+	my $add_io = IO::String->new;
+	my $old_handle = select($add_io);
+
+	&$closure();
+
+	select($old_handle);
+
+	is($expected, ${$add_io->string_ref}, $testName);
+
+	undef $add_io;
+}
+
+1;
