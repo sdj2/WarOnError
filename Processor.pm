7858637465205f07c5b274233a42d78b855051c6
Seth Johnson <seth.johnson@gmail.com>
2013-02-21 01:34:46 -0800

    Rudimentary Processor module

--- Processor.pm
+++ Processor.pm
@@ -1,3 +1,38 @@
 package WarOnError::Processor;
 
-0; #not built yet
+use warnings;
+use strict;
+use Exporter qw(import);
+
+our @EXPORT_OK = qw(new);
+
+sub new {
+	my $package = $_[0];
+	my %processor = ('classifier' => undef);
+	bless \%processor, $package;
+}
+
+sub process {
+	my ($processor, $message) = @_;
+	my $classify = $processor->{classifier};
+
+	if (!defined($classify)) {
+		return 0;
+	}
+
+	my @handlers = $classify->($message);
+
+	foreach (@handlers) {
+		if (!$message) {
+			last;
+		}
+		$message = $_->($message);
+	}
+}
+
+sub setClassifier {
+	my ($processor, $classifier) = @_;
+	$processor->{classifier} = $classifier;
+}
+
+1; #not built yet
